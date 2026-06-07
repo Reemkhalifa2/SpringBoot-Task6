43bd9b06c3116bf9fd541c5abfbf0ce6ca23d335
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-07 14:25:30 +0400

    Task 32: Building a Java Spring Boot UPDATE Endpoint – Product Stock Update Example

--- src/main/java/com/example/Task6Demo/ProductController.java
+++ src/main/java/com/example/Task6Demo/ProductController.java
@@ -12,12 +12,14 @@ import java.util.Map;
 public class ProductController {
 
     HashMap<Integer, Product> products = new HashMap<>();
+    public ProductController() {
+        products.put(1, new Product(101, "Laptop", 20));
+        products.put(2, new Product(201, "Iphone", 45));
+        products.put(3, new Product(301, "Monitor", 15));
+    }
 
     @PutMapping("/updateStock/{id}")
     public String updateStock(@PathVariable int id, @RequestParam int stock){
-        products.put(1, new Product(1, "Laptop", 20));
-        products.put(2, new Product(2, "Iphone", 45));
-        products.put(3, new Product(3, "Monitor", 15));
         for(Map.Entry<Integer,Product > m : products.entrySet()){
             if(m.getValue().getId() == id){
                 int prevStock = m.getValue().getStockQuantity();
