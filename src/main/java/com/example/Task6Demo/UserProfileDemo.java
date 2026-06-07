4499df5a802f0c3558d52b03ac57092fd56723fc
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-07 13:02:00 +0400

    Task 31: Applying Java OOP for UPDATE Operations – User Profile Update Example

---
+++ src/main/java/com/example/Task6Demo/UserProfileDemo.java
@@ -0,0 +1,16 @@
+package com.example.Task6Demo;
+
+public class UserProfileDemo {
+    public static void main(String[] args) {
+        UserProfile userProfile = new UserProfile(1001,"0712345678","Address: Muscat, Oman");
+        System.out.println("ID: "+userProfile.getUserId());
+        System.out.println("Phone Number: "+userProfile.getPhoneNumber());
+        System.out.println("Address: "+userProfile.getAddress());
+        System.out.println();
+        userProfile.setPhoneNumber("0798765432");
+        userProfile.setAddress("Ruwi, Muscat ");
+        System.out.println("Phone Number changed to: "+userProfile.getPhoneNumber());
+        System.out.println("Address changed to: "+userProfile.getAddress());
+
+    }
+}
